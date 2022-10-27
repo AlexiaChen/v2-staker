ab131efe0808c97b879faf9b28021093f4877153
MathxH Chen <brainfvck@foxmail.com>
2022-10-27 19:11:01 +0800

    update deploy config

--- scripts/deployv2.ts
+++ scripts/deployv2.ts
@@ -5,11 +5,38 @@ import { ethers } from 'hardhat';
 async function main() {
 
     
-    const [deployer, addr1, addr2] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+  const [owner, addr1, addr2] = await ethers.getSigners();
+    
+  //// UniswapV2ERC20 token
+  const uniswapERC20Token = await ethers.getContractFactory("UniswapV2ERC20");
+  const hreUniswapERC20Token = await uniswapERC20Token.deploy();
+  await hreUniswapERC20Token.deployed();
+
+  //// StakingRewardsFactory
+
+  // mock reward token
+  const mockRewardToken = await ethers.getContractFactory("MyRewardERC20");
+  const hreMockReward = await mockRewardToken.deploy();
+  await hreMockReward.deployed();
+
+  // mock staking token
+  const mockStakingToken = await ethers.getContractFactory("MyStakingERC20");
+  const hreMockStaking = await mockStakingToken.deploy();
+  await hreMockStaking.deployed();
+
+  // getting timestamp
+  const blockNumBefore = await ethers.provider.getBlockNumber();
+  const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+  const timestamp = blockBefore.timestamp + 60;
+
+  const stakingRewardsFactory = await ethers.getContractFactory("StakingRewardsFactory");
+  const hreStakingRedwardsFactory = await stakingRewardsFactory.deploy(hreMockReward.address, timestamp);
+  await hreStakingRedwardsFactory.deployed();
+
+  console.log("My Reward ERC20 Token address is:", hreMockReward.address);
+  console.log("My Staking ERC20 Token address is:", hreMockStaking.address); 
+  console.log("UniswapV2ERC20 address is:", hreUniswapERC20Token.address);
+  console.log("StakingRewardsFactory address is:", hreStakingRedwardsFactory.address);
     
   
   }
